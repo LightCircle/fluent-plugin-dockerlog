6e136aa317ba2bdea6ab5af03490210f30a668bf
LI LIN <r2space@gmail.com>
2016-11-25 14:36:28 +0800

    初期导入

--- lib/fluent/plugin/out_dockerlog.rb
+++ lib/fluent/plugin/out_dockerlog.rb
@@ -1,9 +1,32 @@
-require "fluent/plugin/dockerlog/version"
-
 module Fluent
-  module Plugin
-    module Dockerlog
-      # Your code goes here...
+
+  class TypecastOutput < Output
+    Fluent::Plugin.register_output('typecast', self)
+
+    # Define `log` method for v0.10.42 or earlier
+    unless method_defined?(:log)
+      define_method("log") { $log }
+    end
+
+    def initialize
+      super
+      # require 'hogepos'
+      log.info "a"
+    end
+
+    def configure(conf)
+      super
+      log.info "b"
+    end
+
+    def start
+      super
+      log.info "c"
+    end
+
+    def shutdown
+      super
+      log.info "d"
     end
   end
 end
